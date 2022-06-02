078e044c62745bcd6606c36793c657e266e53fb8
drapiigi <82909800+drapiigi@users.noreply.github.com>
2022-06-02 11:07:06 +0000

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,22 +7,21 @@ unsigned int binary_to_uint(const char *b)
     int index = strlen(b)-1;
     if(b == NULL)//if b is null
     {             
-        return 0;
+        return (0);
     }
 
     for (index;index >=0;index--)
     {
         if (b[index] !='1' || b[index]!='0')   //if b is neither 0 or 1 
         {
-            return 0;
+            return (0);
         }
        
        if ( b[index]=='1' )
         {
-            total += multiplier;
-           
+            total += multiplier
         }
         multiplier *= 2;
     }
-    return total;
+    return (total);
 }
